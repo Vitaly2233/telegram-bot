8cf581ced4f7feeddcadfce65d2af94c31d477b3
Vitaly2233 <vitalyryabchich@gmail.com>
2023-08-15 21:31:11 +0200

    gay percent change

--- controllers/basic-controllers/how-gay-test.ts
+++ controllers/basic-controllers/how-gay-test.ts
@@ -2,10 +2,18 @@ import { CommandContext } from "../../models/command-context";
 import { randomNumber } from "../../utils/random-number";
 
 export const howGayTest = async (ctx: CommandContext) => {
-  const gayPercent = randomNumber(0, 101);
+  let gayPercent = 0;
+  let attempts = 0;
+  while (gayPercent !== 101) {
+    gayPercent = randomNumber(0, 101);
+    attempts++;
+  }
   const messageId = ctx.message.message_id;
 
-  await ctx.reply(`Ти гей на ${gayPercent}%`, {
-    reply_to_message_id: messageId,
-  });
+  await ctx.reply(
+    `Ти гей на ${gayPercent}%, На це пішло ${attempts} спроб щоб довести`,
+    {
+      reply_to_message_id: messageId,
+    }
+  );
 };
